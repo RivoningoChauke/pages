c77ea39a6f684680353588283a50618b3c31652b
Rivoningo Chauke <mashambarm26@gmail.com>
2023-05-31 09:19:20 +0200

    .net debugging

--- AlpineSkiHouse/Program.cs
+++ AlpineSkiHouse/Program.cs
@@ -1,4 +1,26 @@
+using Humanizer;
 var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
 
 // Add services to the container.
 builder.Services.AddRazorPages();
